2a3b51c0996cdbc47b6eac904b08360394520feb
Shantanu Bhadoria <shantanu@cpan.org>
2015-05-20 17:50:44 +0800

    FIrst commit, working set of rulers

---
+++ js/ssvg.js
@@ -0,0 +1,15 @@
+/*
+SSVG SVG Elements drawing Library
+Author: Shantanu Bhadoria
+*/
+
+//Create HTML Around canvas element
+$( document ).ready(function(){
+  $('.ssvg-canvas').ruler({
+    showCrosshair: false
+  });  
+});
+
+
+
+//var paper = Raphael()
